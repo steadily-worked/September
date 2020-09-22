8e8da6909c0860e8cddb58c6a462da14c1da391d
sangminpark <qpwpep5429@gmail.com>
2020-09-22 18:47:55 +0900

    divide js from html

---
+++ JavaScript/VanillaJS/maplestory/js/tag.js
@@ -0,0 +1,45 @@
+// /* JavaScript markdown editor SimpleMDE */
+// var simplemde = new SimpleMDE({ element: document.getElementById("MyID") });
+
+
+/* add Tag by click or press enter */
+var elements = document.getElementById('element');
+var taskSubmit = document.getElementById('btn_add_task');
+var taskBox = document.getElementById('text_task');
+var taskList = document.getElementById('list_tasks');
+
+
+document.addEventListener('keydown', function(e) {
+    var task = taskBox.value.trim();
+    var newLI = document.createElement('li');
+    var removeBtn = document.createElement('button');
+    var element = newLI.appendChild(document.createTextNode(task));
+    if ((taskBox.value != "") && (e.keyCode === 188 || e.keyCode === 13 || e.keyCode === 32))  {
+        e.preventDefault();
+        taskList.appendChild(newLI);
+        taskList.appendChild(removeBtn);
+        taskBox.value = '';
+        removeBtn.addEventListener('click', function() {
+            removeBtn.parentNode.removeChild(removeBtn);
+            newLI.parentNode.removeChild(newLI);
+        });
+    }
+}, false);
+document.addEventListener('click', function(e) {
+    var task = taskBox.value.trim();
+    var newLI = document.createElement('li');
+    var removeBtn = document.createElement('button');
+    removeBtn.innerHTML = "❌";
+    var element = newLI.appendChild(document.createTextNode(task));
+    if ((taskBox.value != "") && (e.keyCode === 188 || e.keyCode === 13 || e.keyCode === 32))  {
+        e.preventDefault();
+        taskList.appendChild(newLI);
+        taskList.appendChild(removeBtn);
+        taskBox.value = '';
+        removeBtn.addEventListener('click', function() {
+            removeBtn.parentNode.removeChild(removeBtn);
+            newLI.parentNode.removeChild(newLI);
+        });
+    }
+}, false);
+$('#list_tasks').on('')
