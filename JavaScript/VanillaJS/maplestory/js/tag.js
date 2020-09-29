95744cc3bbdb0e2836ee77b0cbd087e26e4a7d11
sangminpark <qpwpep5429@gmail.com>
2020-09-29 16:33:52 +0900

    finish preventing input other than languages and numbers

--- JavaScript/VanillaJS/maplestory/js/tag.js
+++ JavaScript/VanillaJS/maplestory/js/tag.js
@@ -5,28 +5,23 @@
 /* add Tag by click or press enter, comma, space */
 var elements = document.getElementById('element');
 var taskSubmit = document.getElementById('btn_add_task');
-var taskBox = document.getElementById('text_task');
+var taskBox = document.querySelector('#text_task');
 var taskList = document.getElementById('list_tasks');
 
-var enterKey = 13;
-var tabKey = 9;
-var commaKey = 188;
-var spaceKey = 32;
+/* Prevent input other than Korean, English and numbers */
+taskBox.addEventListener('keyup',  removeSpecial);
+function removeSpecial (e) {
+    e.target.value = e.target.value.replace(/[^ㄱ-힣a-zA-Z0-9]/gi,"");
+}
 
-var addPrevents = ['', ',', ' '];
-var addTriggers = [enterKey, tabKey, commaKey, spaceKey];
-var tags = [];
-
-document.addEventListener('keydown', function(e) {
+/* keyup */
+document.addEventListener('keyup', function(e) {
     var keyCode = e.keyCode;
     var task = taskBox.value.trim();
     var newLI = document.createElement('li');
     var removeBtn = document.createElement('button');
     var element = newLI.appendChild(document.createTextNode(task));
-    if (!addTriggers.includes(keyCode)) {
-        return;
-    }
-    if ((taskBox.value != "") && (taskBox.value.charAt(taskBox.length-1) != ",") && (e.keyCode === 188 || e.keyCode === 13 || e.keyCode === 32))  {
+    if ((taskBox.value != "") && (e.keyCode === 188 || e.keyCode === 13 || e.keyCode === 32))  {
         e.preventDefault();
         taskList.appendChild(newLI);
         taskList.appendChild(removeBtn);
@@ -37,14 +32,13 @@ document.addEventListener('keydown', function(e) {
         });
     } 
 }, false);
-// endswith를 적용한 결과, ,은 무한으로 생기고 마지막에 ,이 아닌 다른 게 있을 경우에 추가된다.
-// 내가 하고싶었던 건 아예 콤마를 값에 추가시키지 못하게 하는 것.
+
 document.addEventListener('click', function(e) {
     var task = taskBox.value.trim();
     var newLI = document.createElement('li');
     var removeBtn = document.createElement('button');
     var element = newLI.appendChild(document.createTextNode(task));
-    if ((taskBox.value != "") && (taskBox.value.charAt(taskBox.length-1) != ",") && (e.keyCode === 188 || e.keyCode === 13 || e.keyCode === 32))  {
+    if ((taskBox.value != "") && (e.keyCode === 188 || e.keyCode === 13 || e.keyCode === 32))  {
         e.preventDefault();
         taskList.appendChild(newLI);
         taskList.appendChild(removeBtn);
@@ -55,5 +49,3 @@ document.addEventListener('click', function(e) {
         });
     }
 }, false);
-
-// text_value.endswith(",") : e.preventDefault(); 뭐 이런식으로 ..
