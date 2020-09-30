434314ac33ad3a38b776ea882f38eb397d077a26
sangminpark <qpwpep5429@gmail.com>
2020-09-30 15:57:47 +0900

    add autocomplete using jQuery

--- JavaScript/VanillaJS/maplestory/js/tag.js
+++ JavaScript/VanillaJS/maplestory/js/tag.js
@@ -11,11 +11,12 @@ var taskList = document.getElementById('list_tasks');
 /* Prevent input other than Korean, English and numbers */
 taskBox.addEventListener('keyup',  removeSpecial);
 function removeSpecial (e) {
-    e.target.value = e.target.value.replace(/[^ㄱ-힣a-zA-Z0-9]/gi,"");
+    e.target.value = e.target.value.replace(/[^ㄱ-힣a-zA-Z0-9+#]/gi,"");
 }
 
 /* keyup */
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', keyupFunction, false);
+function keyupFunction(e) {
     var keyCode = e.keyCode;
     var task = taskBox.value.trim();
     var newLI = document.createElement('li');
@@ -31,21 +32,37 @@ document.addEventListener('keyup', function(e) {
             newLI.parentNode.removeChild(newLI);
         });
     } 
-}, false);
+}
 
-document.addEventListener('click', function(e) {
-    var task = taskBox.value.trim();
-    var newLI = document.createElement('li');
-    var removeBtn = document.createElement('button');
-    var element = newLI.appendChild(document.createTextNode(task));
-    if ((taskBox.value != "") && (e.keyCode === 188 || e.keyCode === 13 || e.keyCode === 32))  {
-        e.preventDefault();
-        taskList.appendChild(newLI);
-        taskList.appendChild(removeBtn);
-        taskBox.value = '';
-        removeBtn.addEventListener('click', function() {
-            removeBtn.parentNode.removeChild(removeBtn);
-            newLI.parentNode.removeChild(newLI);
-        });
-    }
-}, false);
+$(function() {
+	var languages = [
+		"ActionScript", "AppleScript", "Asp","BASIC", "C",
+		"C++", "Clojure", "COBOL", "ColdFusion", "Erlang",
+		"Fortran", "Groovy", "Haskell", "Java", "JavaScript",
+		"Lisp", "Perl", "PHP", "Python", "Ruby",
+		"Scala", "Scheme"
+	];
+
+	$( "#text_task" ).autocomplete({
+		source: languages
+	});
+});
+
+
+/* Autocomplete using jQuery*/
+
+// $(function() {
+//     var availableLanguages = ['파이썬','Django', 'Python', 'JavaScript', 'Java', 'Go' ,'C', 'C#', 'C++'];
+//     $('#text_task').autocomplete({
+//         source: availableLanguages,
+//         select: function(event, ui) {
+//             console.log(ui.item);
+//         },
+//         focus: function(event, ui) {
+//             return false;
+//             //event.preventDefault();
+//         },
+//         minLength: 1,
+//         autoFocus: true,
+//     });
+// });
